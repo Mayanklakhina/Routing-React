db54d06b78adb60bccccbd55771c05843bd45c5a
Mayank <mayank.lakhina@cloudeq.com>
2023-08-04 11:59:31 +0530

    Created Routing features using React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Navigation from './Navigation';
+import Portfolio from './Portfolio';
+import Entertainment from './Entertainment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+      <Routes>
+        <Route path='/' element={<Navigation />} />
+        <Route path="/home" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        
+        <Route exact path="/portfolio" element={<Portfolio />} />
+        
+        <Route exact path="/entertainment" element={<Entertainment />} />
+      </Routes>
+
+      </Router>
   );
 }
 
